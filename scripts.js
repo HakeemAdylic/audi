41bd21c98677a3b841a847b2f55e927c9eaf287c
Hakeem Ayeketi <hakeem.ayeketi@adylic.com>
2024-03-22 09:48:52 +0000

    Remove redundant code

--- scripts.js
+++ scripts.js
@@ -76,129 +76,12 @@
 })();
 
 // Middle
-(function () {
-  function isDevEnv() {
-    return (
-      window.location.href.indexOf('orca.adylic.com') > 0 ||
-      window.location.href.indexOf('template.adylicorca.com') > 0 ||
-      window.location.href.indexOf('saturn.adylic.com') > 0 ||
-      window.location.hostname === '127.0.0.1' ||
-      window.location.hostname === 'localhost'
-    );
-  }
-  function devLog(message) {
-    if (isDevEnv()) console.log(message);
-  }
-
-  function checkForGsap(callback) {
-    let DEADLINE_MS = 10000;
-    let RETRY_MS = 100;
-    let interval;
-
-    let timeout = setTimeout(() => {
-      clearInterval(interval);
-      devLog('GSAP failed to load after ' + DEADLINE_MS / 1000 + ' second(s).');
-    }, DEADLINE_MS);
-
-    try {
-      if (gsap) {
-        clearTimeout(timeout);
-        callback();
-      }
-    } catch {
-      devLog('Initial GSAP load failed. Trying again.');
-      interval = setInterval(() => {
-        try {
-          if (gsap) {
-            clearTimeout(timeout);
-            clearInterval(interval);
-            callback();
-          }
-        } catch {}
-      }, RETRY_MS);
-    }
-  }
-
-  function initGlobalAnimations() {
-    const DURATION = 0.5;
-    const tl = gsap.timeline();
-
-    const currentFrame = '.middle-frame';
-
-    //  Define element selectors
-    const headlineSelector = `${currentFrame} .headline`;
-    const subheadlineSelector = `${currentFrame} .subheadline`;
-
-    tl.to(currentFrame, { opacity: 1, duration: DURATION }) // end time: 0.5
-      .fromTo(headlineSelector, { x: '-100%' }, { x: 0, duration: DURATION }) // end time: 1
-      .fromTo(
-        subheadlineSelector,
-        { x: '-100%' },
-        { x: 0, duration: DURATION },
-        '+=1'
-      ) // end time: 2.5
-      .to(
-        [headlineSelector, subheadlineSelector],
-        { x: '100%', duration: DURATION },
-        '+=3'
-      ) // end time: 5.5
-      .to(currentFrame, {
-        opacity: 0,
-        visibility: 'hidden',
-        duration: DURATION,
-      }); // end time: 6
-  }
-  checkForGsap(initGlobalAnimations);
-})();
+KGZ1bmN0aW9uICgpIHsKICBmdW5jdGlvbiBpbml0R2xvYmFsQW5pbWF0aW9ucygpIHsKICAgIGNvbnN0IERVUkFUSU9OID0gMC41OwogICAgY29uc3QgdGwgPSBnc2FwLnRpbWVsaW5lKCk7CgogICAgY29uc3QgY3VycmVudEZyYW1lID0gJy5taWRkbGUtZnJhbWUnOwoKICAgIC8vICBEZWZpbmUgZWxlbWVudCBzZWxlY3RvcnMKICAgIGNvbnN0IGhlYWRsaW5lU2VsZWN0b3IgPSBgJHtjdXJyZW50RnJhbWV9IC5oZWFkbGluZWA7CiAgICBjb25zdCBzdWJoZWFkbGluZVNlbGVjdG9yID0gYCR7Y3VycmVudEZyYW1lfSAuc3ViaGVhZGxpbmVgOwoKICAgIHRsLnRvKGN1cnJlbnRGcmFtZSwgeyBvcGFjaXR5OiAxLCBkdXJhdGlvbjogRFVSQVRJT04gfSkgLy8gZW5kIHRpbWU6IDAuNQogICAgICAuZnJvbVRvKGhlYWRsaW5lU2VsZWN0b3IsIHsgeDogJy0xMDAlJyB9LCB7IHg6IDAsIGR1cmF0aW9uOiBEVVJBVElPTiB9KSAvLyBlbmQgdGltZTogMQogICAgICAuZnJvbVRvKAogICAgICAgIHN1YmhlYWRsaW5lU2VsZWN0b3IsCiAgICAgICAgeyB4OiAnLTEwMCUnIH0sCiAgICAgICAgeyB4OiAwLCBkdXJhdGlvbjogRFVSQVRJT04gfSwKICAgICAgICAnKz0xJwogICAgICApIC8vIGVuZCB0aW1lOiAyLjUKICAgICAgLnRvKAogICAgICAgIFtoZWFkbGluZVNlbGVjdG9yLCBzdWJoZWFkbGluZVNlbGVjdG9yXSwKICAgICAgICB7IHg6ICcxMDAlJywgZHVyYXRpb246IERVUkFUSU9OIH0sCiAgICAgICAgJys9MycKICAgICAgKSAvLyBlbmQgdGltZTogNS41CiAgICAgIC50byhjdXJyZW50RnJhbWUsIHsKICAgICAgICBvcGFjaXR5OiAwLAogICAgICAgIHZpc2liaWxpdHk6ICdoaWRkZW4nLAogICAgICAgIGR1cmF0aW9uOiBEVVJBVElPTiwKICAgICAgfSk7IC8vIGVuZCB0aW1lOiA2CiAgfQogIGluaXRHbG9iYWxBbmltYXRpb25zKCk7Cn0pKCk7
 
 // End
 (function () {
-  function isDevEnv() {
-    return (
-      window.location.href.indexOf('orca.adylic.com') > 0 ||
-      window.location.href.indexOf('template.adylicorca.com') > 0 ||
-      window.location.href.indexOf('saturn.adylic.com') > 0 ||
-      window.location.hostname === '127.0.0.1' ||
-      window.location.hostname === 'localhost'
-    );
-  }
-
-  function devLog(message) {
-    if (isDevEnv()) console.log(message);
-  }
-
-  function checkForGsap(callback) {
-    let DEADLINE_MS = 10000;
-    let RETRY_MS = 100;
-    let interval;
-
-    let timeout = setTimeout(() => {
-      clearInterval(interval);
-      devLog('GSAP failed to load after ' + DEADLINE_MS / 1000 + ' second(s).');
-    }, DEADLINE_MS);
-
-    try {
-      if (gsap) {
-        clearTimeout(timeout);
-        callback();
-      }
-    } catch {
-      devLog('Initial GSAP load failed. Trying again.');
-      interval = setInterval(() => {
-        try {
-          if (gsap) {
-            clearTimeout(timeout);
-            clearInterval(interval);
-            callback();
-          }
-        } catch {}
-      }, RETRY_MS);
-    }
-  }
-
   function initGlobalAnimations() {
     const DURATION = 0.5;
-
     const currentFrame = '.end-frame';
 
     //  Define element selectors
@@ -216,8 +99,8 @@
           scale: config.scale,
         });
       },
-      defaults: { duration: 0.125, scale: 1 }, //defaults get applied to any "config" object passed to the effect
-      extendTimeline: true, //now you can call the effect directly on any GSAP timeline to have the result immediately inserted in the position you define (default is sequenced at the end)
+      defaults: { duration: 0.125, scale: 1 },
+      extendTimeline: true,
     });
 
     tl.to(currentFrame, { opacity: 1, duration: DURATION }) // end time: 0.5
@@ -235,5 +118,6 @@
       .pulse(ctaSelector, { scale: 1.05 })
       .pulse(ctaSelector, { scale: 1 });
   }
-  checkForGsap(initGlobalAnimations);
+
+  initGlobalAnimations();
 })();
